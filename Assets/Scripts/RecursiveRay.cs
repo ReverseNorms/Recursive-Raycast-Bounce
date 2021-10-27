b547f7303c67dece7082e4187bec957b40bb0224
Invertex <Invertex@users.noreply.github.com>
2021-10-27 05:21:22 -0700

    Avoid stack overflow, 2021.2 upgrade, ray limit variable
    Reworked the raycast recursion to use feedback instead so as to avoid any potential for a stack overflow.
    Upgraded project to 2021.2.
    Implemented variable raycast density.

--- Assets/Scripts/RecursiveRay.cs
+++ Assets/Scripts/RecursiveRay.cs
@@ -1,50 +1,74 @@
-﻿//RecursiveRaycast test by Invertex
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
-public class RecursiveRay : MonoBehaviour {
-	[SerializeField]LayerMask layerMask = 1;
-	[SerializeField]float maxRayTravel = 20f;
+
+[ExecuteAlways]
+public class RecursiveRay : MonoBehaviour
+{
+	[SerializeField] LayerMask layerMask = 1;
+	[SerializeField, Range(0, 1000)] int raysToCast = 360;
+	[SerializeField] float maxRayTravel = 20f;
 	RaycastHit hit;
+
 	public bool increaserays = false;
-	void OnValidate(){
-		if(increaserays)
+	void OnValidate()
+	{
+		if (increaserays)
 		{
 			increaserays = false;
 			StartCoroutine(RayIncrease());
 		}
-
 	}
 
-	IEnumerator RayIncrease(){
-		while(maxRayTravel < 50f){
+	IEnumerator RayIncrease()
+	{
+		while (maxRayTravel < 50f)
+		{
 			maxRayTravel += Time.deltaTime * 5;
 			yield return null;
 		}
 	}
 
-	void Update(){
+	void Update()
+	{
 		Vector3 rot = transform.localEulerAngles;
-		for(int i = 0; i < 360; i++){
-			transform.localEulerAngles = new Vector3(rot.x, (float)i, rot.z);
-			RaycastRecursion(transform.position, transform.forward, maxRayTravel, new Color(1f,1f,1f));
-			Debug.Log(transform.forward);
+		float rotIncrement = 1f / raysToCast;
+
+		for (int i = 0; i < raysToCast; i++)
+		{
+			float angle = rotIncrement * i;
+			transform.localEulerAngles = new Vector3(rot.x, angle * 360, rot.z);
+			RaycastRecursion(transform.position, transform.forward, maxRayTravel, new Color(1f, 1f, 1f));
 		}
 		transform.localEulerAngles = rot;
 	}
 
-	void RaycastRecursion(Vector3 position, Vector3 dir, float maxDist, Color col){
-		if(Physics.Raycast(position, dir, out hit, maxDist, layerMask)) 
+	void RaycastRecursion(Vector3 position, Vector3 dir, float maxDist, Color col)
+	{
+		while(maxDist > 0)
+        {
+			Raycast(ref position, ref dir, ref maxDist, col);
+			col.g -= 0.25f; col.b -= 0.25f;
+		}
+	}
+	void Raycast(ref Vector3 position, ref Vector3 dir, ref float maxDist, Color col)
+	{
+		if (Physics.Raycast(position, dir, out hit, maxDist, layerMask))
 		{
 			float dist = Vector3.Distance(position, hit.point);
 			Debug.DrawRay(position, dir.normalized * dist, col);
+
 			maxDist -= dist;
-			if(maxDist > 0) {
-				col.g -= 0.25f; col.b -= 0.25f;
-				RaycastRecursion(hit.point, Vector3.Reflect(dir, hit.normal), maxDist, col);
+
+			if (maxDist > 0)
+			{
+				position = hit.point;
+				dir = Vector3.Reflect(dir, hit.normal);
 			}
 		}
-		else{ //Disabled this part if you only want to see rays that have hit something
+		else
+		{ //Disabled this part if you only want to see rays that have hit something
 			Debug.DrawRay(position, dir.normalized * maxDist, col);
+			maxDist = 0f;
 		}
 	}
 }
